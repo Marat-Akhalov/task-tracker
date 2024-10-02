fc2c7e7a8b99d0270ad98eeeb757eb6f4a0a1011
Marat-Akhalov <117514563+Marat-Akhalov@users.noreply.github.com>
2024-10-02 18:11:03 +0400

    feat: add header component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-
+import TheHeader from "./components/layout/TheHeader.vue";
 </script>
 
 <template>
-
+  <TheHeader title="Task Tracker App" />
 </template>
 
 <style scoped></style>
---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    default: 'Default Header Text'
+  }
+})
+</script>
+
+<template>
+  <header>
+    <h2 class="header">
+      {{ title }}
+    </h2>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  margin: 0;
+  font-size: 1.75rem;
+}
+</style>
