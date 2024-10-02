5495c83ddc2bc6852001b6a31e193894af0a6f8b
Marat-Akhalov <117514563+Marat-Akhalov@users.noreply.github.com>
2024-10-02 17:42:35 +0400

    init: init project

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import '@/assets/sass/main.scss'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+const store = createPinia()
+
+app.use(store)
+app.use(router)
+
+app.mount('#app')
